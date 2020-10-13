f0f65bdbb4038e52bc4488ad504f50c0609351ad
pajid9763 <pajid9763@wrdsb.ca>
2020-10-13 13:14:26 +0000

    method for random number of stars

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
 /**
- *
- * @author 
+ * method for random number of stars
+ * @author Darian
  */
 public class Main {
+  public static void chaotic(int num){
+    int randNum = (int)(Math.random()*(5 - 1 + 1))+ 1;
+    // for loop will keep repeating until i becomes less than chosen number
+    for (int i = 0; i < num; i++){
+      
+      // on 1 line print "*" until the number of stars reaches randNum
+      for (int j = 0; j < randNum; j++){
+        randNum = (int)(Math.random()*(5 - 1 + 1))+ 1;
+        System.out.print("*");
+      }
+      // print an empty line so the next row of stars can be printed
+      System.out.println("");
+    }
+
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // call out chaotic method
+    chaotic(7);
     
   }
 }
